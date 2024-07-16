5c2ff54d38ad412d12ff54ffa2e0c53f7750d0d2
Wahaj-Khan <108826496+Wahaj-Khan@users.noreply.github.com>
2024-07-17 02:50:10 +0500

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { FaMoon } from "react-icons/fa"
 const Header = () => {
     const path = useLocation().pathname
     return (
-        <Navbar className="border-b-2 w-full">
+        <Navbar className="border-b-2 p-4 w-full">
             <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-lg font-semibold dark:text-white">
                 <span className="px-2 py-1 rounded-lg text-white bg-gradient-to-r from-purple-500 to-blue-500">
                     Wahaj&apos;s
@@ -35,13 +35,13 @@ const Header = () => {
             </div>
             <Navbar.Collapse>
                 <Navbar.Link active={path === '/'} as={'div'}>
-                    <Link to="/">Home</Link>
+                    <Link className="text-base" to="/">Home</Link>
                 </Navbar.Link>
                 <Navbar.Link active={path === '/about'} as={'div'}>
-                    <Link to="/about">About</Link>
+                    <Link className="text-base" to="/about">About</Link>
                 </Navbar.Link>
                 <Navbar.Link active={path === '/projects'} as={'div'}>
-                    <Link to="/projects">Projects</Link>
+                    <Link className="text-base" to="/projects">Projects</Link>
                 </Navbar.Link>
             </Navbar.Collapse>
         </Navbar>
