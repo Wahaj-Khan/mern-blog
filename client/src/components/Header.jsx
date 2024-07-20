a30dd9c2b0d352c7870cecc1fdc8b8c6183c9e3e
Wahaj-Khan <108826496+Wahaj-Khan@users.noreply.github.com>
2024-07-20 23:51:06 +0500

    add profile dropdown in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,16 @@
-import { Button, Navbar, TextInput } from "flowbite-react"
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
 import { Link, useLocation } from "react-router-dom"
 import { AiOutlineSearch } from "react-icons/ai"
 import { FaMoon } from "react-icons/fa"
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { useSelector } from "react-redux"
+
 const Header = () => {
+    const { currentUser } = useSelector(state => state.user)
     const path = useLocation().pathname
     return (
         <Navbar className="border-b-2 p-4 w-full">
-            <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-lg font-semibold dark:text-white">
+            <Link to="/" className="ml-6 self-center whitespace-nowrap text-sm sm:text-lg font-semibold dark:text-white">
                 <span className="px-2 py-1 rounded-lg text-white bg-gradient-to-r from-purple-500 to-blue-500">
                     Wahaj&apos;s
                 </span>
@@ -22,15 +26,53 @@ const Header = () => {
             <Button className="w-12 h-10 items-center lg:hidden" color='gray' pill>
                 <AiOutlineSearch />
             </Button>
-            <div className="flex gap-2 md:order-2">
-                <Button className="w-12 h-10 items-center hidden sm:inline" color='gray' pill>
-                    <FaMoon/>
+            <div className="flex mr-6 gap-4 md:order-2">
+                <Button className="w-14 h-12 items-center hidden sm:inline" color='gray' pill>
+                    <FaMoon size={16} />
                 </Button>
-                <Link to="/sign-in">
-                    <Button className="items-center" gradientDuoTone='purpleToBlue' outline>
-                        Sign In
-                    </Button>
-                </Link>
+
+                {currentUser ? (
+                    <Dropdown
+                        arrowIcon={false}
+                        inline
+                        label={
+                            <Avatar
+                                size='md'
+                                rounded
+                                alt="user"
+                                img={currentUser.profilePicture}
+                            />
+                        }
+                    >
+                        <Link to={'/dashboard?tab=profile'}>
+                            <Dropdown.Item className="flex items-center font-bold text-base">
+                                <Avatar
+                                    size='sm'
+                                    rounded
+                                    alt="user"
+                                    img={currentUser.profilePicture}
+                                />
+                                <span className="ml-2">{currentUser.username}</span>
+                            </Dropdown.Item>
+                        </Link>
+                        <Dropdown.Divider />
+                        <Link to="#">
+                            <Dropdown.Item className="font-medium text-base">
+                                <FaArrowRightFromBracket className="mr-4 w-5 h-5"/>
+                                Sign out
+                            </Dropdown.Item>
+                        </Link>
+
+                    </Dropdown>
+                ) : (
+                    <Link to="/sign-in">
+                        <Button className="items-center" gradientDuoTone='purpleToBlue' outline>
+                            Sign In
+                        </Button>
+                    </Link>
+
+                )
+                }
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUer: null,
+    currentUser: null,
     error: null,
     loading: false,
 }
@@ -16,7 +16,7 @@ export const userSlice = createSlice({
         },
         signInSuccess: (state, action) => {
             state.loading = false;
-            state.currentUer = action.payload;
+            state.currentUser = action.payload;
             state.error = null;
         },
         signInFailure: (state, action) => {
