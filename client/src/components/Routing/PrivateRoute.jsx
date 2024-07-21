b96d9944cf7e5493e5876b87c626f44c73d90d54
Wahaj-Khan <108826496+Wahaj-Khan@users.noreply.github.com>
2024-07-21 21:24:46 +0500

    update private route

--- client/src/components/Routing/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-const PrivateRoute = ({ element }) => {
-  const currentUser = useSelector(state => state.user.currentUser);
-  return currentUser ? element : <Navigate to="/sign-in" replace />;
-};
-
-export default PrivateRoute;
--- client/src/components/Routing/PublicRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-const PublicRoute = ({ element }) => {
-  const currentUser = useSelector(state => state.user.currentUser);
-  return !currentUser ? element : <Navigate to="/" replace />;
-};
-
-export default PublicRoute;
